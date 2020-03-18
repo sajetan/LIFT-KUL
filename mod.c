83229b05d21a012baec7bbd660e34746cca35ea2
LienWou <62055250+LienWou@users.noreply.github.com>
2020-03-18 10:57:38 +0100

    Add files via upload

---
+++ mod.c
@@ -0,0 +1,110 @@
+/*
+
+ * mod.c
+ *
+ *  Created on: Mar 17, 2020
+ *      Author: r0665956
+ */
+
+#include"mod.h"
+
+// Calculates res = a mod n
+// a and n represent large integers stored in arrays with WORD-type elements
+// a is an array of length_a elements, n is an array of length_n elements, res has min{length_a,length_n} - elements
+void mod(WORD *res, WORD *a, WORD *n)
+{
+	WORD length_a = a[0];
+	WORD length_n = n[0];
+
+
+	//z = index of most significant non zero element of a
+	WORD z = length_a;
+	while(a[z] == 0){
+			z--;
+			}
+	//y = index of most significant non zero element of n
+	WORD y = length_n;
+	while(n[y] == 0){
+			y--;
+			}
+
+	// if y > z => n > a => res = a
+	if (y>z){
+		res[0] = length_a;
+		WORD i;
+		for(i=1; i<=length_a; i++) {
+			res[i] = a[i];
+		}
+	}
+
+	// if z > y => a > n
+	if(z>y) {
+
+		while (z>y){
+			// a = a - n
+			sub(a,a,n);
+			//z = index of most significant non zero element of a
+			WORD z = length_a;
+			while(a[z] == 0){
+					z--;
+					}
+			//y = index of most significant non zero element of n
+			WORD y = length_n;
+			while(n[y] == 0){
+					y--;
+					}
+		}
+	}
+
+	if (z==y){
+		//if a[z] < n[z] => a < n => res = a
+		if (a[z] < n[z]){
+			WORD i;
+			for(i=1; i<=length_a; i++) {
+				res[i] = a[i];
+					}
+		}
+
+		//if a[z] > n[z] => a > n
+		if(a[z] > n[z]){
+			while (a[z] > n[z]) {
+				// a =  a - n
+				sub(a,a,n);
+				}
+			WORD i;
+			for(i=1; i<=length_a; i++) {
+				res[i] = a[i];
+					}
+		}
+
+
+		if (a[z] == n[z]){
+			while (a[z]==n[z]){
+			// z = index of most significant element in a and n that are not equal
+				z--;
+			}
+
+			//if a[z] < n[z] => a < n => res = a
+			if (a[z] < n[z]){
+				WORD i;
+				for(i=1; i<=length_a; i++) {
+							res[i] = a[i];
+						}
+				}
+			//if a[z] > n[z] => a > n
+			if (a[z] > n[z]) {
+				while (a[z] > n[z]) {
+					// a =  a - n
+					sub(a,a,n);
+						}
+				WORD i;
+				for(i=1; i<=length_a; i++) {
+					res[i] = a[i];
+						}
+
+			}
+			}
+	}
+}
+
+

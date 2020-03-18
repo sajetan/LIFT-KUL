b982dcb20b4b7f02328581dfa05b56c04dae33a0
LienWou <62055250+LienWou@users.noreply.github.com>
2020-03-18 16:05:18 +0100

    Update mod.c

--- mod.c
+++ mod.c
@@ -28,15 +28,6 @@ void mod(WORD *res, WORD *a, WORD *n)
 			y--;
 			}
 
-	// if y > z => n > a => res = a
-	if (y>z){
-		res[0] = length_a;
-		WORD i;
-		for(i=1; i<=length_a; i++) {
-			res[i] = a[i];
-		}
-	}
-
 	// if z > y => a > n
 	if(z>y) {
 
@@ -54,16 +45,22 @@ void mod(WORD *res, WORD *a, WORD *n)
 					y--;
 					}
 		}
+		//after while-loop, you are sure that z <= y
+
+	// if y > z => n > a => res = a
+	if (y>z){
+		res[0] = length_a;
+		WORD i;
+		for(i=1; i<=length_a; i++) {
+			res[i] = a[i];
+		}
+	}
+
+
+
 	}
 
 	if (z==y){
-		//if a[z] < n[z] => a < n => res = a
-		if (a[z] < n[z]){
-			WORD i;
-			for(i=1; i<=length_a; i++) {
-				res[i] = a[i];
-					}
-		}
 
 		//if a[z] > n[z] => a > n
 		if(a[z] > n[z]){
@@ -71,32 +68,49 @@ void mod(WORD *res, WORD *a, WORD *n)
 				// a =  a - n
 				sub(a,a,n);
 				}
+		}
+		//after while-loop, you are sure that a[z]<= n[z]
+
+		//if a[z] < n[z] => a < n => res = a
+		if (a[z] < n[z]){
+			res[0] = length_a;
 			WORD i;
 			for(i=1; i<=length_a; i++) {
 				res[i] = a[i];
 					}
 		}
 
-
 		if (a[z] == n[z]){
-			while (a[z]==n[z]){
-			// z = index of most significant element in a and n that are not equal
+			while (a[z]==n[z] && z!=2){
+			// z = index of most significant element of a and n that are not equal
 				z--;
 			}
 
-			//if a[z] < n[z] => a < n => res = a
-			if (a[z] < n[z]){
+
+			// if z = 2 => a = n => res = 0
+			if (z==2){
+				res[0] = length_a;
 				WORD i;
 				for(i=1; i<=length_a; i++) {
-							res[i] = a[i];
+					res[i] = 0;
+						}
+			}
+
+
+			//if a[z] < n[z] => a < n => res = a
+			if (a[z] < n[z]){
+				res[0] = length_a;
+				WORD i;
+				for(i=1; i<=length_a; i++) {
+					res[i] = a[i];
 						}
 				}
 			//if a[z] > n[z] => a > n
 			if (a[z] > n[z]) {
-				while (a[z] > n[z]) {
-					// a =  a - n
-					sub(a,a,n);
-						}
+				// a =  a - n
+				sub(a,a,n);
+					}
+				res[0] = length_a;
 				WORD i;
 				for(i=1; i<=length_a; i++) {
 					res[i] = a[i];

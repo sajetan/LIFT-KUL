ed9da9aeeadd792fa11bfa2c7b1718f6c1b244f8
LienWou <62055250+LienWou@users.noreply.github.com>
2020-03-18 16:03:08 +0100

    Add files via upload

---
+++ main.h
@@ -0,0 +1,21 @@
+/*
+ * main.h
+ *
+ *  Created on: Mar 17, 2020
+ *      Author: r0665956
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define WORD uint32_t
+#define WORD_2 uint64_t
+
+#include<stdio.h>
+#include<stdint.h>
+
+#include"add.h"
+#include"sub.h"
+#include"mod.h"
+
+#endif /* MAIN_H_ */

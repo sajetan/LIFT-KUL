c9ff9ef60859630732fc2176a5467e0918e21625
FerdinandHannequart <60980877+FerdinandHannequart@users.noreply.github.com>
2020-03-25 17:22:45 +0100

    Add files via upload

---
+++ mod.h
@@ -0,0 +1,15 @@
+/*
+ * mod.h
+ *
+ *  Created on: Mar 17, 2020
+ *      Author: r0665956
+ */
+
+#ifndef MOD_H_
+#define MOD_H_
+
+#include"main.h"
+
+void mod(WORD *res, WORD *a, WORD *n);
+
+#endif /* MOD_H_ */

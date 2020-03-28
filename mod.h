38ed012c840bfc9294c11ae862374548fae93277
LienWou <62055250+LienWou@users.noreply.github.com>
2020-03-28 14:47:25 +0100

    Create mod.h

---
+++ mod.h
@@ -0,0 +1,20 @@
+/*
+ * mod.h
+ *
+ *  Created on: Mar 28, 2020
+ *      Author: r0665956
+ */
+
+#ifndef MOD_H_
+#define MOD_H_
+
+#include"main.h"
+
+void mod(WORD *res, WORD *a, WORD *n);
+
+//testfunctions
+void modTest(WORD print);
+void modTestHelp(char aChar[], char bChar[], char expChar[], WORD* pass, WORD print);
+
+
+#endif /* MOD_H_ */

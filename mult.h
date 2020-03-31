ccb6fcedce3c445237e111a95deb79c87c0da43f
LienWou <62055250+LienWou@users.noreply.github.com>
2020-03-31 15:56:18 +0200

    added smallMult

--- mult.h
+++ mult.h
@@ -1,5 +1,7 @@
 
- /* mult.h
+
+/*
+ * mult.h
  *
  *  Created on: Mar 28, 2020
  *      Author: r0665956
@@ -11,10 +13,14 @@
 #include"main.h"
 
 void mult(WORD *res, WORD *a, WORD *b);
+void smallMult(WORD *res, WORD a, WORD *b);
 
 //testfunctions
 
 void multTest(WORD print);
 void multTestHelp(char aChar[], char bChar[], char expChar[], WORD* pass, WORD print);
+void smallMultTest(WORD print);
+void smallMultTestHelp(WORD a, char bChar[], char expChar[], WORD* pass, WORD print);
 
 #endif /* MULT_H_ */
+

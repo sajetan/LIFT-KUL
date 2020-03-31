4fa7fd35b1683bd01814a4f1e5983f391b91b776
LienWou <62055250+LienWou@users.noreply.github.com>
2020-03-31 15:58:36 +0200

    added smallMult

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ void main(){
 	addSelfTest(0);
 	subTest(0);
 	subSelfTest(0);
- 	getNumberBytesTest(0);
- 	hash256Test(0);
+	getNumberBytesTest(0);
+	hash256Test(0);
     shiftl1Test(0);     // 0 is summary, 1 is everything
     shiftr1Test(0);     // 0 is summary, 1 is everything
     inverseTest(0);
@@ -32,4 +32,5 @@ void main(){
    /////// Lien ///////
 	modTest(0);
 	multTest(0);
+	smallMultTest(0);
 }

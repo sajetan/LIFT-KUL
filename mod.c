6e59eecee0eefa437765242e8b6f50084f1c0f49
LienWou <62055250+LienWou@users.noreply.github.com>
2020-04-02 13:45:14 +0200

    updated with division

--- mod.c
+++ mod.c
@@ -3,6 +3,7 @@
 
  * mod.c
 
+
  *
  *  Created on: Mar 17, 2020
  *      Author: LIEN
@@ -15,6 +16,19 @@
 // a is an array of length_a elements (0-elements not included), n is an array of length_n elements (0-elements not included),
 // res has min{length_a,length_n} - elements (0-elements not included)
 void mod(WORD *res, WORD *a, WORD *n){
+	WORD quotient[SIZE] = {0};
+	if(a[0] == 0){
+		WORD res[SIZE] = {0};
+	}
+	else if(a[0] == 1){
+		smallMod(res, a , n);
+	}
+	else{
+		division(quotient, res, a, n);
+	}
+}
+
+void smallMod(WORD *res, WORD *a, WORD *n){
 
 	while (greaterThan(a,n) == 1){
 		subSelf(a,n);
@@ -45,9 +59,14 @@ void modTest(WORD print){
     modTestHelp("FF", "1", "0", &pass,  print);
     modTestHelp("1", "100", "1", &pass,  print);
     modTestHelp("10100", "FF", "2", &pass,  print);
-    modTestHelp("2F984E58B2B", "1FAFE27CFD","107A9D373", &pass,  print);
-	  modTestHelp("1FAFE27CFD","2F984E58B2B","1FAFE27CFD", &pass,  print);
+    modTestHelp("2F984E58B2B","1FAFE27CFD","107A9D373", &pass,  print);
+	modTestHelp("1FAFE27CFD","2F984E58B2B","1FAFE27CFD", &pass,  print);
     modTestHelp("9E6F6C70F1", "1FAFE27CFD", "0", &pass,  print);
+    modTestHelp("F0B22C70313F84E6528979C359F36917BDFF4B365F81CF5D40AF394EAB27DD440CFF87036D8A972FE2F9E8FDAA8CAF778B7D5014436CF1E73F59834D995A1F2C31",
+    		"1B", "B", &pass,  print);
+    modTestHelp("F0B22C70313F84E6528979C359F36917BDFF4B365F81CF5D40AF394EAB27DD440CFF87036D8A972FE2F9E8FDAA8CAF778B7D5014436CF1E73F59834D995A1F2C31",
+    		"20CF0EA1A1A5FC3A45ACEC29C2ED0B7", "150750C7822DF3796E49AD6E4B99F7F", &pass,  print);
+
 
 
     TEST(pass)

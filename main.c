1dbf45a8ef8acf9dbe3c1fbc65258f078e2f8d54
LienWou <62055250+LienWou@users.noreply.github.com>
2020-04-06 09:59:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -20,11 +20,11 @@ void main(){
 	subTest(0);
 	subSelfTest(0);
 
- 	//getNumberBytesTest(0);
- 	hash256Test(0);
-    shiftl1Test(0);     // 0 is summary, 1 is everything
-    shiftr1Test(0);     // 0 is summary, 1 is everything
-    inverseTest(0);
+	//getNumberBytesTest(0);
+	hash256Test(0);
+	shiftl1Test(0);     // 0 is summary, 1 is everything
+	shiftr1Test(0);     // 0 is summary, 1 is everything
+	inverseTest(0);
 
 	convertTest();
 
@@ -33,7 +33,6 @@ void main(){
 	modTest(0);
 	multTest(0);
 	smallMultTest(0);
-	shiftBaseTest(0);
 	divisionTest(0);
 
 

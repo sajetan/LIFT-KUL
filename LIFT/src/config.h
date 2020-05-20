6205fae47e64c6cceb044377ff8eb25c1793162a
FerdinandHannequart <60980877+FerdinandHannequart@users.noreply.github.com>
2020-05-20 06:41:21 +0200

    Add files via upload

--- LIFT/src/config.h
+++ LIFT/src/config.h
@@ -12,22 +12,26 @@
 #define DEBUG 0 //enable to print logs
 #define P(print)    printf("hello %x \n", print);
 
-#define PRINT_STATE()   if(1)\
+#define PRINT_STATE()   if(1)\ 						// set this to 1 if you want to see the current state
 		printf("[ Current STATE:  %s ]\n", __func__);
-#define DEBUG_FSM(s)    if(0)\
+#define DEBUG_FSM(s)    if(0)\						// set this to 1 if you want to see additional info about the FSM (useful for debugging)
 		printf("\t%s \n",s);
 #define PRINT_VIDEO_FRAME(s,l)    if(1)\
 		printf("\tVideo Seq num [%d] len [%d] \n",s,l);
 
-#define DEBUG_SIGNATURE 0
-#define PRINT_CONTENT_UDP_PACKET 0
+#define DEBUG_SIGNATURE 0							// set this to 1 if you want to print out all values related to the STS protocol
+#define PRINT_CONTENT_UDP_PACKET 0					// content packets
+#define PRINT_BITFLIP() if(1)\						// Interesting! set this to 1 if you want to get notified when CRC detects a bit flip.
+						printf("CRC Incorrect, Packet will be dropped --------------------\n"); 
+
 
 #define MAX_TRANSFER_LENGTH  200 //1472 is the usual max but can be changed
-
-#define BER_INVERSE  5000        // max 32000 min 1
 #define SEND_CONSTANT_DATA 1 //for integration to send constant data
-#define IF_BITERROR 1 //enable or disable biterrors
-#define SIMULATE_PACKET_DROP 0 // max 100, set to zero to disable
 #define VIDEOFRAMES 4 //each of 32bytes
+#define DELAY_VIDEO 0 // in µs, sets the delay between video frames in case the communication cannot follow
+
+#define IF_BITERROR 1 			//enable or disable bit errors
+#define BER_INVERSE  4000       // max 32000 min 1, represents the inverse of the desired BER
+#define SIMULATE_PACKET_DROP 0 	// max 100, set to zero to disable
 
 #endif /* CONFIG_H_ */

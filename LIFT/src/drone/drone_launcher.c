a432e93a15b6a71b5ee2b3d5d5dfb89b3f84ce2b
FerdinandHannequart <60980877+FerdinandHannequart@users.noreply.github.com>
2020-05-20 07:02:51 +0200

    Update drone_launcher.c

--- LIFT/src/drone/drone_launcher.c
+++ LIFT/src/drone/drone_launcher.c
@@ -11,8 +11,8 @@
 #include "drone_includes.h"
 
 //destination IP and port numbers
-//static const char CC_IP[] = "10.87.20.93";
-static const char CC_IP[] = "127.0.0.1";
+static const char CC_IP[] = "10.87.20.93";
+//static const char CC_IP[] = "127.0.0.1";
 static int TX_PORT = 9997;
 static int RX_PORT = 9996;
 
